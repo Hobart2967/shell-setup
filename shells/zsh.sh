eec466e69e82c184a462af1b1a13f03f5b14af71
Marco Klein <marco.klein@trustedshops.de>
2022-07-11 15:33:11 +0200

    feat: :sparkles: Introduce autosuggestion and syntax highlighting for zsh

--- shells/zsh.sh
+++ shells/zsh.sh
@@ -1,9 +1,9 @@
 if [[ ! -z "$ZSH" ]]; then
+  ZSH_DIR=$HOME/.oh-my-zsh
 
   #########
   # Theme #
   #########
-
   if [[ ! -f "$ZSH/custom/themes/agnoster-ex.zsh-theme" ]]; then
     echo "$COLORS_YELLOW\t ==> Downloading zsh theme$COLORS_RESET"
     rm -rf ~/source/github.com/Hobart2967/agnoster-ex
@@ -12,9 +12,27 @@ if [[ ! -z "$ZSH" ]]; then
     cp ~/source/github.com/Hobart2967/agnoster-ex/agnoster-ex.zsh-theme "$ZSH/custom/themes/agnoster-ex.zsh-theme"
   fi
 
+
+  ###############
+  # Autosuggest #
+  ###############
+  ZSH_AUTOSUGGEST_DIR="$ZSH_DIR/custom/plugins/zsh-autosuggestions"
+  if [ ! -d "$ZSH_AUTOSUGGEST_DIR" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_AUTOSUGGEST_DIR
+  fi
+
+  ####################
+  # Syntax Highlight #
+  ####################
+  ZSH_SYNTAX_HL_DIR="$ZSH_DIR/custom/plugins/zsh-syntax-highlighting"
+  if [ ! -d "$ZSH_SYNTAX_HL_DIR" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX_HL_DIR
+  fi
+
   ##########
   # Config #
   ##########
   ZSH_THEME=agnoster-ex
   DEFAULT_USER=$(whoami)
+  plugins=(aws git history docker-compose docker gh awsume zsh-autosuggestions zsh-syntax-highlighting)
 fi
