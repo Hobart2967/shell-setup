bd0b581b28f69663e9d5b39a280a3b4759baaa8b
Marco Klein <marco.klein@trustedshops.de>
2022-09-08 09:22:59 +0200

    fix: :bug: Fix elanor's MAC Address

--- tools/wake-on-lan.sh
+++ tools/wake-on-lan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "$OSTYPE" =~ ^darwin.* ]]; then
-  alias wakeup-elanor="wakeonlan 00:2564:DD:D9:6D"
+  alias wakeup-elanor="wakeonlan 00:25:64:DD:D9:6D"
 else
-  alias wakeup-elanor="etherwake 00:2564:DD:D9:6D"
+  alias wakeup-elanor="etherwake 00:25:64:DD:D9:6D"
 fi
