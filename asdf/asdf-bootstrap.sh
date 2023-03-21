27f88128fe45762caaa0395affe4ea0cfee1ae17
Hobart2967 <hobart@codewyre.net>
2023-03-22 00:01:53 +0100

    feat(asdf-vm): :sparkles: Add installation request for asdf

--- asdf/asdf-bootstrap.sh
+++ asdf/asdf-bootstrap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ ! -d "$HOME/.asdf" ]; then
+  printf "\n\n\t$COLORS_BLUE =>$COLORS_GREEN Asdf Version Manager$COLORS_BLUE was not installed yet.$COLORS_GREEN Install it?$COLORS_BLUE (y[es]/n[o]) [Default: n]$COLORS_RESET "
+  read yn
+  if [[ "$yn" == "y" ]]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+  fi
+fi
+
 if [ -d "$HOME/.asdf" ]; then
   #source "/opt/homebrew/opt/asdf/libexec/asdf.sh"
   . ~/.asdf/plugins/java/set-java-home.zsh
