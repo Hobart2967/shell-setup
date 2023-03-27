3ea7025a39b7baef666f4f5ed14082242f37a72d
Marco Klein <marco.klein@trustedshops.de>
2023-03-27 13:22:20 +0200

    feat(zsh): :sparkles: Adjust terminal to show Core team instead of platform team

--- utilities/greet.sh
+++ utilities/greet.sh
@@ -1,20 +1,29 @@
 if [ "$MACHINE_TYPE" = "work" ]; then
+  WORK_RAINBOW=("${RAINBOW[@]:0:8}")
+
+  printf '%s _________ %s       %s        %s           %s ___________%s        %s        %s         %s \n' $WORK_RAINBOW $RESET
+  printf '%s \_   ___ \%s    ____ %s _______%s    ____ %s \__    ___/%s  ____ %s _____  %s   _____ %s \n' $WORK_RAINBOW $RESET
+  printf '%s /    \  \/%s   /  _ \%s \_  __ \%s _/ __ \%s   |    |  %s _/ __ \%s \__  \ %s  /     \%s \n' $WORK_RAINBOW $RESET
+  printf '%s \     \____%s (  <_> )%s |  | \/%s \  ___/%s   |    |  %s \  ___/%s  / __ \_%s |  Y Y \%s \n' $WORK_RAINBOW $RESET
+  printf '%s  \______  /%s  \____/%s  |__|  %s   \___  >%s  |____|  %s  \__  >%s (____  /%s|__|_| /%s \n' $WORK_RAINBOW $RESET
+  printf '%s         \/%s       %s        %s         \/%s           %s     \/%s       \/%s      \/%s \n' $WORK_RAINBOW $RESET
   printf '\n'
-  printf '%s__________%s.__  %s     %s   __  %s  _____ %s     %s       %s        %s ___________ %s             %s   %s   %s\n' $RAINBOW $RESET
-  printf '%s\\______   \\%s  | %s_____ %s_/  |__%s/ ____\\%s_____%s______ %s _____  %s \\__    ___/%s___ %s_____   %s _____  %s\n' $RAINBOW $RESET
-  printf '%s |     ___/%s  | %s\\__  \\%s\\   __%s\\   __\\%s/  _ \\%s_  __ \\%s/     \\ %s   |    |%s_/ __ \\%s\\__  \\  %s/     \\ %s\n' $RAINBOW $RESET
-  printf '%s |    |   %s|  |__%s/ __ \\%s|  |  %s|  | %s(  <_> )%s  | \\%s/  Y Y  \\ %s  |    |%s\\  ___/ %s/ __ \\%s|  Y Y  \\%s\n' $RAINBOW $RESET
-  printf '%s |____|   %s|____%s(____  /%s__|  %s|__|  %s\\____/%s|__|  %s|__|_|  /  %s |____|%s \\___  >%s____  /%s__|_|  /%s\n' $RAINBOW $RESET
-  printf '%s          %s     %s     \\/ %s       %s            %s      %s    \\/      %s       %s  \\/   %s %s \\/   %s   \\/ %s \n' $RAINBOW $RESET
+  #printf '%s__________%s.__  %s     %s   __  %s  _____ %s     %s       %s        %s ___________ %s             %s   %s   %s\n' $RAINBOW $RESET
+  #printf '%s\\______   \\%s  | %s_____ %s_/  |__%s/ ____\\%s_____%s______ %s _____  %s \\__    ___/%s___ %s_____   %s _____  %s\n' $RAINBOW $RESET
+  #printf '%s |     ___/%s  | %s\\__  \\%s\\   __%s\\   __\\%s/  _ \\%s_  __ \\%s/     \\ %s   |    |%s_/ __ \\%s\\__  \\  %s/     \\ %s\n' $RAINBOW $RESET
+  #printf '%s |    |   %s|  |__%s/ __ \\%s|  |  %s|  | %s(  <_> )%s  | \\%s/  Y Y  \\ %s  |    |%s\\  ___/ %s/ __ \\%s|  Y Y  \\%s\n' $RAINBOW $RESET
+  #printf '%s |____|   %s|____%s(____  /%s__|  %s|__|  %s\\____/%s|__|  %s|__|_|  /  %s |____|%s \\___  >%s____  /%s__|_|  /%s\n' $RAINBOW $RESET
+  #printf '%s          %s     %s     \\/ %s       %s            %s      %s    \\/      %s       %s  \\/   %s %s \\/   %s   \\/ %s \n' $RAINBOW $RESET
 else
+  HOME_RAINBOW=("${RAINBOW[@]:0:11}")
   printf '\n'
   printf '\n'
-  printf '%s  ___ ___  %s     %s___.   %s      %s       %s  __ /\ %s      %s  ___ ___  %s        %s       %s       %s\n' $RAINBOW $RESET
-  printf '%s /   |   \ %s ____%s\_ |__ %s_____ %s_______%s_/  |)/%s______ %s /   |   \ %s ____   %s_____  %s ____  %s\n' $RAINBOW $RESET
-  printf '%s/    ~    \%s/  _ \%s| __ \%s\__  \%s\_  __ \%s   __/%s  ___/%s /    ~    \%s/  _ \ %s/     \_%s/ __ \ %s\n' $RAINBOW $RESET
-  printf '%s\    Y    %s(  <_> %s| \_\ \%s/ __ \%s|  | \/%s|  | %s\___ \ %s \    Y    %s(  <_> |%s  Y Y  \%s  ___/ %s\n' $RAINBOW $RESET
-  printf '%s \___|_  / %s\____/%s|___  %s(____  |%s__|   %s|__|%s/____  > %s \___|_  / %s\____/|%s__|_|  /%s\___  >%s\n' $RAINBOW $RESET
-  printf '%s       \/  %s      %s    \/ %s    \/ %s       %s   %s     \/   %s      \/   %s       %s    \/  %s   \/ %s\n' $RAINBOW $RESET
+  printf '%s  ___ ___  %s     %s___.   %s      %s       %s  __ /\ %s      %s  ___ ___  %s        %s       %s       %s\n' $HOME_RAINBOW $RESET
+  printf '%s /   |   \ %s ____%s\_ |__ %s_____ %s_______%s_/  |)/%s______ %s /   |   \ %s ____   %s_____  %s ____  %s\n' $HOME_RAINBOW $RESET
+  printf '%s/    ~    \%s/  _ \%s| __ \%s\__  \%s\_  __ \%s   __/%s  ___/%s /    ~    \%s/  _ \ %s/     \_%s/ __ \ %s\n' $HOME_RAINBOW $RESET
+  printf '%s\    Y    %s(  <_> %s| \_\ \%s/ __ \%s|  | \/%s|  | %s\___ \ %s \    Y    %s(  <_> |%s  Y Y  \%s  ___/ %s\n' $HOME_RAINBOW $RESET
+  printf '%s \___|_  / %s\____/%s|___  %s(____  |%s__|   %s|__|%s/____  > %s \___|_  / %s\____/|%s__|_|  /%s\___  >%s\n' $HOME_RAINBOW $RESET
+  printf '%s       \/  %s      %s    \/ %s    \/ %s       %s   %s     \/   %s      \/   %s       %s    \/  %s   \/ %s\n' $HOME_RAINBOW $RESET
   printf '\n'
 fi
 
