ba751f2fa797aaa373a159afabf49cfc1d9fe368
Hobart2967 <hobart2967@outlook.com>
2023-06-26 00:12:35 +0200

    fix(python): :bug: Fix crash on non-python prepared environments

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -1,3 +1,8 @@
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+PYENV_RESULT=$(command -v pyenv)
+if [ ! -z "$PYENV_RESULT" ]; then
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+else
+  echo "PyEnv Not installed. Skipping initialization. Install it using 'curl https://pyenv.run | bash'"
+fi
