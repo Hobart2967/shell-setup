faae15624bd78759f10fb2b589487f069968b026
Marco Klein <marco@belonio.de>
2023-08-24 08:20:24 +0200

    feat(git): :sparkles: Add shortcut to update from another branch and pull request command for all team members

--- git/gh.sh
+++ git/gh.sh
@@ -6,6 +6,23 @@ gh-pr() {
   echo "Copied PR URL to Clipboard!"
 }
 
+gh-pra() {
+  CMD="gh pr create -f $PR_TEAM_MEMBERS -B $1"
+  URL=$($(echo $CMD))
+  echo $URL | pbcopy
+  echo $URL
+  echo "Copied PR URL to Clipboard!"
+}
+
+git-update-from() {
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git stash
+  git checkout $1
+  git pull
+  git checkout $CURRENT_BRANCH
+  git merge $1
+}
+
 gh-release() {
   gh release create $1 -t "$1" -n "$1"
 }
