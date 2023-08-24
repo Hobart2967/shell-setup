aa06c06fa001841951eff5053ff3de658cd6fa5c
Marco Klein <marco@belonio.de>
2023-08-24 08:22:18 +0200

    feat(aws): :sparkles: Add shortcut for aws-sso-util to login first

--- aws-tools/awsume.sh
+++ aws-tools/awsume.sh
@@ -1,1 +1,16 @@
-alias awsume=". awsume"
+binPath=$(command -v awsume)
+if [ ! -z "$binPath" ]; then
+  _awsume() {
+      local cur prev opts
+      COMPREPLY=()
+      cur="${COMP_WORDS[COMP_CWORD]}"
+      prev="${COMP_WORDS[COMP_CWORD-1]}"
+      opts=$(awsume-autocomplete)
+      COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+      return 0
+  }
+  complete -F _awsume awsume
+fi
+alias awsume=". awsume --output-profile default"
+
+alias aws-login="aws-sso-util login --profile $AWS_DEFAULT_ACCOUNT"
