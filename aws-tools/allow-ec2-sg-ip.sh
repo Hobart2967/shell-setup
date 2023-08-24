41c61c547e24c70fdf97225c1c572cdb683d1558
Marco Klein <marco@belonio.de>
2023-08-24 08:46:21 +0200

    feat(aws): :sparkles: Implement update mechanism for security groups

--- aws-tools/allow-ec2-sg-ip.sh
+++ aws-tools/allow-ec2-sg-ip.sh
@@ -4,10 +4,15 @@
 function allow-ec2-sg-ip() {
   COUNT="$#"
   if [[ $COUNT -lt 2 ]]; then
-    echo "Usage: allow-ec2-sg-ip <HOST> <PORT>"
+    echo "Usage: allow-ec2-sg-ip <HOST> <PORT> <PROTOCOL=tcp>"
   else
     SECURITY_GROUP_ID=$1
     PORT=$2
+    PROTOCOL=$3
+    if [ -z "$PROTOCOL" ]; then
+      PROTOCOL=tcp
+    fi
+
 
     USER=$(whoami)
     HOST=$(hostname)
@@ -15,10 +20,34 @@ function allow-ec2-sg-ip() {
 
     PUBLIC_IP=$(curl -s -4 https://ipinfo.io | jq .ip -r)
 
-    echo "Allowing ${PORT}:${PORT} on EC2 Security Group ${SECURITY_GROUP_ID} for IP ${PUBLIC_IP}/32"
+    echo "Find existing rule for $USER@HOST"
+    RULE_ID=$(aws ec2 describe-security-group-rules --filter '{ "Name": "group-id", "Values": [ "'$JENKINS_SG'" ] }' --query "max(SecurityGroupRules[?Description==\`$USER@$HOST\`].SecurityGroupRuleId)" | sed 's/"//g' )
+    if [ "$RULE_ID" = "null" ]; then
+      RULE_ID=""
+      echo "Cannot find existing rule. Creating..."
+    else
+      echo "Found existing rule. Updating..."
+    fi
 
-    aws ec2 authorize-security-group-ingress \
+    echo "Allowing ${PORT}:${PORT} on EC2 Security Group ${SECURITY_GROUP_ID} for IP ${PUBLIC_IP}/32"
+    if [ -z "$RULE_ID" ]; then
+      RESULT=$(aws ec2 authorize-security-group-ingress \
+          --group-id ${SECURITY_GROUP_ID} \
+          --ip-permissions FromPort=${PORT},ToPort=${PORT},IpProtocol=${PROTOCOL},IpRanges="[{CidrIp="${PUBLIC_IP}/32",Description='${USER}@${HOST}'}]")
+    else
+       RESULT=$(aws ec2 modify-security-group-rules \
         --group-id ${SECURITY_GROUP_ID} \
-        --ip-permissions FromPort=${PORT},ToPort=443,IpProtocol=tcp,IpRanges="[{CidrIp="${PUBLIC_IP}/32",Description='${USER}@${HOST} at ${DATE}'}]"
+        --security-group-rules '[{
+          "SecurityGroupRuleId": "'$RULE_ID'",
+          "SecurityGroupRule": {
+            "IpProtocol": "tcp",
+            "FromPort": '${PORT}',
+            "ToPort": '${PORT}',
+            "CidrIpv4": "'${PUBLIC_IP}'/32",
+            "Description": "'${USER}'@'${HOST}'"
+          }
+        }]')
+    fi
+
   fi
 }
