c659b17f24be2444694043f3bf51dc76c4df8a2a
Hobart2967 <hobart2967@outlook.com>
2025-03-05 01:02:04 +0100

    fix(1password): :bug: Improve one-password bin path resolution

--- wsl/one-password.sh
+++ wsl/one-password.sh
@@ -1,5 +1,3 @@
-export OP_PATH="/mnt/c/Users/marco/AppData/Local/Microsoft/WinGet/Packages/AgileBits.1Password.CLI_Microsoft.Winget.Source_8wekyb3d8bbwe/op.exe"
-
 if [ ! -f /usr/local/bin/op ]; then
   chmod a+x $PERSONAL_SHELL_SETUP_PATH/wsl/op
   sudo cp $PERSONAL_SHELL_SETUP_PATH/wsl/op /usr/local/bin/op
